e5bbe8b1adddddf8d909b76afed85dfd13427dad
darren-kk <rlawoghks10@gmail.com>
2022-11-28 01:39:04 +0900

    add block multi dot functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,52 @@
-const numbers = document.querySelectorAll(".num")
-const blank = document.querySelector(".blank")
+const num1Element = document.querySelector("#num1")
+const num2Element = document.querySelector("#num2")
+const num3Element = document.querySelector("#num3")
+const num4Element = document.querySelector("#num4")
+const num5Element = document.querySelector("#num5")
+const num6Element = document.querySelector("#num6")
+const num7Element = document.querySelector("#num7")
+const num8Element = document.querySelector("#num8")
+const num9Element = document.querySelector("#num9")
+const num0Element = document.querySelector("#num0")
+const numdotElement = document.querySelector("#numdot")
+const inputElement = document.querySelector(".input_blank")
+const numsElement = document.querySelectorAll(".num")
+const addElement = document.querySelector('#add')
+let numClickResult1 = "";
 
-for (let i = 0; i < numbers.length; i++) {
-  numbers[i].addEventListener('click', numClick);
-  }
+const numClick = function(event) {
+  numClickResult1 += event.target.textContent;
+  console.log(numClickResult1);
+  inputElement.value = numClickResult1;
 
-  function numClick(event) {
-    blank.textContent = `${event.target.textContent}`;
+  if(numClickResult1.includes('.')) {
+    numdotElement.removeEventListener('click', numClick);
   }
+  
+  return numClickResult1;
+}
+
+num1Element.addEventListener('click', numClick)
+num2Element.addEventListener('click', numClick)
+num3Element.addEventListener('click', numClick)
+num4Element.addEventListener('click', numClick)
+num5Element.addEventListener('click', numClick)
+num6Element.addEventListener('click', numClick)
+num7Element.addEventListener('click', numClick)
+num8Element.addEventListener('click', numClick)
+num9Element.addEventListener('click', numClick)
+num0Element.addEventListener('click', numClick)
+numdotElement.addEventListener('click', numClick)
+
+
+
+
+/* 
+function add(result1, result2) {
+  let addre = result1 + result2
+  return add
+}
+
+
+
+*/
