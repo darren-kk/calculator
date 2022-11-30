804dfed8a50d9bf4da68638f47021ad8e22e8b98
darren-kk <rlawoghks10@gmail.com>
2022-12-01 06:19:44 +0900

    fix calculate functions and errors

--- src/index.js
+++ src/index.js
@@ -9,23 +9,23 @@ let numClickResult1 = "";
 let numClickResult2 = "";
 let symbols = undefined;
 
-const numClick = function (event) {
+const numClick = function(event) {
   if (symbols === undefined) {
-    numClickResult1 += event.target.textContent;
-    inputElement.value = numClickResult1;  
-    
-    if (numClickResult1.includes('.')) {
+    if (numClickResult1.includes(".")) {
       numdotElement.removeEventListener('click', numClick);
-    }
+    } 
+    
+    numClickResult1 += event.target.textContent;
+    inputElement.value = numClickResult1;
     
     return numClickResult1;
   }
-
+  
   if (symbols !== undefined) {
+    numdotElement.addEventListener("click", numClick);
+
     numClickResult2 += event.target.textContent;
     inputElement.value = numClickResult2;  
-
-    numdotElement.addEventListener("click", numClick);
     
     if (numClickResult2.includes('.')) {
       numdotElement.removeEventListener('click', numClick);
@@ -35,14 +35,15 @@ const numClick = function (event) {
   }
 }
 
-const clearHandler = function () {
-  inputElement.value = "";
+const clearHandler = function() {
+  numdotElement.addEventListener("click", numClick);
+  inputElement.value = 0;
   numClickResult1 = "";
   numClickResult2 = "";
   symbols = undefined;
 }
 
-const calculateHandler = function (event) {
+const calculateHandler = function(event) {
   if (event.target) {
     symbols = event.target.textContent;
   }
@@ -50,41 +51,41 @@ const calculateHandler = function (event) {
   return symbols;
 }
 
-const equalHandler = function () {
+const equalHandler = function() {
   let result = 0;
 
   if (symbols === undefined) {
-    inputElement.value = Number(numClickResult1);
+    inputElement.value = Number(numClickResult1.toFixed(8));
   }
 
   switch (symbols) {
     case '+':
-      result = Number(numClickResult1) + Number(numClickResult2);
-      numClickResult1 = result;
+      result = (Number(numClickResult1) + Number(numClickResult2)).toFixed(8);
+      numClickResult1 = result
       numClickResult2 = "";
       symbols = undefined;
-      inputElement.value = result;
+      inputElement.value = Number(result);
     break;
     case '-':
-      result = Number(numClickResult1) - Number(numClickResult2);
+      result = (Number(numClickResult1) - Number(numClickResult2)).toFixed(8);
       numClickResult1 = result;
       numClickResult2 = "";
       symbols = undefined;
-      inputElement.value = result;
+      inputElement.value = Number(result);
     break;
     case 'x':
-      result = Number(numClickResult1) * Number(numClickResult2);
+      result = (Number(numClickResult1) * Number(numClickResult2)).toFixed(8);
       numClickResult1 = result;
       numClickResult2 = "";
       symbols = undefined;
-      inputElement.value = result;
+      inputElement.value = Number(result);
     break;
     case '%':
-      result = Number(numClickResult1) / Number(numClickResult2);
+      result = (Number(numClickResult1) / Number(numClickResult2)).toFixed(8);
       numClickResult1 = result;
       numClickResult2 = "";
       symbols = undefined;
-      inputElement.value = result;
+      inputElement.value = Number(result);
     break;
   }
 }
@@ -97,6 +98,6 @@ sybolElementList.forEach(element => {
   element.addEventListener("click", calculateHandler);
 });
 
-numCElement.addEventListener('click', clearHandler);
 numdotElement.addEventListener("click", numClick);
+numCElement.addEventListener('click', clearHandler);
 equalElement.addEventListener('click', equalHandler);
